792d0433f9579f61735247f103c6b7273bc89e0b
camilashirinli <136453129+camilashirinli@users.noreply.github.com>
2023-11-06 17:16:40 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,139 +1,88 @@
 const tasksBox = document.querySelector(".tasks");
 const input = document.querySelector(".inp");
-const text = document.querySelector(".task__content");
 const btnAdd = document.querySelector(".add");
 let listCheck = document.querySelector('.listCheck');
-let li = document.querySelector('.li')
 let image1 = document.querySelector('.image1')
+
 function addNewElement() {
     tasksBox.style.padding = "10px 0";
     const tempInp = document.createElement("li");
     tempInp.classList.add("task");
     tempInp.innerHTML = `<p class="task__content">${input.value}</p>
-          <button onclick="this.parentElement.remove()" class="remove">
-              <svg class="remove-sv" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <rect x="0.5" y="0.5" width="19" height="19" rx="9.5" stroke="#C4C4C4"/>
-                  <path d="M6 6L14 14" stroke="#C4C4C4"/>
-                  <path d="M6 14L14 6" stroke="#C4C4C4"/>
-              </svg>
-          </button>`;
+      <img src='./xCL.svg' onclick="this.parentElement.remove()" class="remove">
+      <img src='./x.svg' onclick="this.parentElement.remove()" class=" remove removeDel">
+      `;
     input.value = "";
-    tasksBox.insertBefore(tempInp, tasksBox.lastElementChild);
-    remove = document.querySelector(".remove");
-    remove.addEventListener('click', ()=>{
-if(tasks.childElementCount == 0){
-    tasks.style.display = 'none';
-};
-    });
 
+
+    tasksBox.appendChild(tempInp);
 
     input.style.display = "none";
     listCheck.style.display = 'none';
-    let tasks = document.querySelector(".tasks");
-      if (tasks.classList.contains("display-none")) {
-        tasks.classList.remove("display-none");
-        
-        // if(tasks.childElementCount == 0){
-        //     tasks.style.display = 'none';
-        //           }
-      }
-      if(tasks.childElementCount !== 0){
-        tasks.style.display = 'block';
-      }
-// console.log(tasks.childElementCount);
-//       if(tasks.childElementCount !== 0){
-// tasks.style.display = 'block';
-//       }
-  }
-  
-  input.addEventListener("keyup", (event) => {
-    if (event.key == "Enter") {
-      addNewElement();
-      
+
+    if (tasksBox.childElementCount !== 0) {
+        tasksBox.style.display = 'block';
     }
-  });
-  
-  btnAdd.addEventListener("click", () => {
+
+
+
+    let image11 =document.querySelector('.removeDel');
+    let image22 =document.querySelector('.remove');
+    image22.addEventListener('mouseover', ()=>{
+       image22.style.display='none';
+       image11.style.display='block';
+    
+    })
+    image22.addEventListener('mouseleave', ()=>{
+        image22.style.display='block';
+        image11.style.display='none';
+     
+     })
+    
+
+}
+
+
+
+
+input.addEventListener("keyup", (event) => {
+    if (event.key == "Enter") {
+        addNewElement();
+    }
+});
+
+btnAdd.addEventListener("click", () => {
+    if(input.value.trim() !== ''){
     if (getComputedStyle(input).display != "none") {
         listCheck.style.display = 'none';
-      addNewElement();
-    } else {
+        addNewElement();
+    }} else {
         listCheck.style.display = 'block';
-
-      input.style.display = "block";
-      listCheck.style.display = 'block';
-     image1.style.top = '10px';
-
-     
+        input.style.display = "block";
+        listCheck.style.display = 'block';
+        image1.style.top = '10px';
     }
-    
-  });
-  image1.addEventListener('click', ()=>{
+
+});
+
+image1.addEventListener('click', () => {
     input.value = '';
-         });
-  
-  const filterBtn = document.querySelector(".filter__btn");
-  let checkDU = false;
-  
-  function sortItems(){
-    let i, switching, b, shouldSwitch, dir, switchcount = 0;
-    switching = true;
-    dir = "asc"; 
-    while (switching) {
-      switching = false;
-      b = tasksBox.getElementsByTagName("li");
-      for (i = 0; i < (b.length - 2); i++) {
-        shouldSwitch = false;
-        
-        if (dir == "asc") {
-          if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-            
-            shouldSwitch = true;
-            break;
-          }
-        } else if (dir == "desc") {
-          if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
-            
-            shouldSwitch= true;
-            break;
-          }
-        }
-      }
-      if (shouldSwitch) {
-        
-        b[i].parentNode.insertBefore(b[i + 1], b[i]);
-        switching = true;
-        switchcount++;
-      } else {
-        if (switchcount == 0 && dir == "asc") {
-          dir = "desc";
-          switching = true;
-        }
-      }
-    }
-  }
-  
-  
-  filterBtn.addEventListener("click", () => {
-    if (checkDU == false) {
-      filterBtn.innerHTML = `<svg class="filter-sv" width="25" height="15" viewBox="0 0 25 15"  xmlns="http://www.w3.org/2000/svg">
-      <rect x="2.5" width="2.5" height="12.5"/>
-      <rect x="10" y="3.75" width="2.5" height="7.5" transform="rotate(-90 10 3.75)"/>
-      <rect x="10" y="8.75" width="2.5" height="10" transform="rotate(-90 10 8.75)"/>
-      <rect x="10" y="13.75" width="2.5" height="15" transform="rotate(-90 10 13.75)"/>
-      <path d="M3.75 15L0.502405 10.3125L6.9976 10.3125L3.75 15Z"/>
-      </svg>`;
-      checkDU = true;
-      sortItems()
-    } else {
-      filterBtn.innerHTML = `<svg class="filter-sv" width="25" height="15" viewBox="0 0 25 15" xmlns="http://www.w3.org/2000/svg">
-      <rect x="5" y="15" width="2.5" height="12.5" transform="rotate(-180 5 15)"/>
-      <rect x="10" y="3.75" width="2.5" height="7.5" transform="rotate(-90 10 3.75)"/>
-      <rect x="10" y="8.75" width="2.5" height="10" transform="rotate(-90 10 8.75)"/>
-      <rect x="10" y="13.75" width="2.5" height="15" transform="rotate(-90 10 13.75)"/>
-      <path d="M3.75 6.55671e-07L6.99759 4.6875L0.502404 4.6875L3.75 6.55671e-07Z"/>
-      </svg>`;
-      checkDU = false;
-      sortItems()
-    }
-  });
+});
+
+const filterBtn = document.querySelector(".filter__btn");
+let checkDU = false;
+
+filterBtn.addEventListener("click", () => {
+    sortItems();
+});
+
+function sortItems() {
+    const items = Array.from(tasksBox.querySelectorAll("li"));
+    items.sort((a, b) => {
+        const textA = a.querySelector(".task__content").textContent.toLowerCase();
+        const textB = b.querySelector(".task__content").textContent.toLowerCase();
+        return checkDU ? textA.localeCompare(textB) : textB.localeCompare(textA);
+    });
+    items.forEach((item) => tasksBox.appendChild(item));
+    checkDU = !checkDU;
+}
